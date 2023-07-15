19b36a904328d4cc5312d90df4d372fd1230080a
Vasu Gupta <vsugupta6397@gmail.com>
2023-07-15 22:53:21 +0530

    eight changed

--- DayEight/1217.java
+++ DayEight/1217.java
@@ -1,20 +1,21 @@
 
-
-class solution{
-	public int minChip(int[] nums){
-		int n = arr.length;
-		int even = 0,odd=0;
-		for(int num : nums){
-			//counting odd and even values
-			if(num%2==0){
-				even++;
-			}else{
-				odd++;
+//simple bubble sort where we check every adjacent element and swap the greater element with the small one.
+class BubbleSort{
+	public void bubble(int[] arr){
+		//chdcking wheather the elements are sorted or not if not then will not run 2nd for loop
+		boolean swap;
+		for(int i =0;i<arr.length;i++){
+			swap = false;
+			for(int j=1;j<arr.length-i-1;j++){
+				if(arr[j]<arr[j-1]){
+					int temp = arr[j-1];
+					arr[j-1] = arr[j];
+					arr[j] = temp;
+					swap = true
+				}
 			}
 		}
-		if(odd == n || even == n){
-			return 0;
+		if(!swap){
+			break;
 		}
-		return Math.min(odd,even);
-	}
 }
